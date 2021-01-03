18680188be16d2cf32fb45d6bcd18d84be77082f
Emily Nguyen <emilyn0614@gmail.com>
2021-01-03 13:26:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -31,6 +31,7 @@ end
 
 def output_array(array)
   array = "Be yourself;", "everyone else is already taken", "- Oscar Wilde"
+  puts array
   array = ["5", "4", "3", "2", "1"]
   puts output_array
 end
