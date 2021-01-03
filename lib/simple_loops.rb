7e0e1121b73ebdd4ba9ff2b87240e2a0d5491c92
Emily Nguyen <emilyn0614@gmail.com>
2021-01-03 13:19:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,36 @@
-# Write your methods here
+def loop_message_five_times(message)
+  message = "Hello World."
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+end
+
+def loop_message_n_times(message, number)
+  message = "Hello Moon."
+  number = 5
+  puts message 
+  puts message 
+  puts message 
+  puts message 
+  puts message 
+  message = "Hello Red Balloon."
+  number = 10 
+  puts message 
+  puts message 
+  puts message 
+  puts message 
+  puts message 
+  puts message 
+  puts message 
+  puts message 
+  puts message 
+  puts message 
+end
+
+def output_array(array, number)
+  array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
+  number = [5, 4, 3, 2, 1]
+  puts output_array
+end
